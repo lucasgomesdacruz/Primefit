518368cdba9a41a6a7c98dfa453bee46ccd57bf4
Lucas Gomes <lucaass1997rp@gmail.com>
2023-12-18 03:03:05 -0300

    correções de codigos

--- js/calculadora.js
+++ js/calculadora.js
@@ -2,10 +2,20 @@ const resultado = document.querySelector('#resultado')
 const altura = document.querySelector('#altura')
 const peso = document.querySelector('#peso')
 
-const calcIMC = () => {
+
+function validarInput(input) {
+  let valor = input.value;
   
+  if (valor.length > 4) {
+      input.value = valor.slice(0, 4);
+  }
+}
+
+const calcIMC = () => {
+
   if (altura.value !== '' && peso.value !== '') {
-    const imc = (peso.value / (altura.value * altura.value)).toFixed(2)
+    let imc = (peso.value / (altura.value * altura.value)).toFixed(2)
+
     let classification = ''
 
     if (imc < 18.5) {
@@ -21,12 +31,16 @@ const calcIMC = () => {
     } else {
       classification = 'Obesidade Grau III'
     }
-    
-    resultado.innerHTML = `IMC: ${imc} (${classification})`
+
     resultado.style.color = "green"
+    imc = imc.replace(/[.-]/g, ',');
+    resultado.innerHTML = `IMC: ${imc} (${classification})`
   } else {
-    resultado.innerHTML = 'Preencha os campos'
+    altura.style.border = '3px solid red'
+    peso.style.border = '3px solid red'
+    resultado.innerHTML = `Preencha os campos corretamente`
     resultado.style.color = "red"
+    
   }
   
   altura.value = ""
