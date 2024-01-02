e89895590530a35d7a71eafa089b85c6dd9c1b26
Lucas Gomes <lucaass1997rp@gmail.com>
2024-01-02 15:32:12 -0300

    correção e adição de novos arquivos

---
+++ js/blog.js
@@ -0,0 +1,38 @@
+let listElement = document.querySelector("#app")
+
+let posts = []
+
+function nutriApp() {
+
+    fetch("https://sujeitoprogramador.com/rn-api/?api=posts")
+    .then((r) => r.json())
+    .then((json) => {
+        posts = json;
+
+        posts.map((item) => {
+            let liElement = document.createElement("li")
+            let titleElement = document.createElement("h2")
+            let imgElement = document.createElement("img")
+            let descriptionElement = document.createElement("p")
+
+            let titleText = document.createTextNode(item.titulo)
+            titleElement.appendChild(titleText)
+            liElement.append(titleElement)
+
+            imgElement.src = item.capa
+            liElement.appendChild(imgElement)
+
+            let descriptionText = document.createTextNode(item.subtitulo)
+            descriptionElement.appendChild(descriptionText)
+            liElement.appendChild(descriptionElement)
+
+            listElement.appendChild(liElement)
+
+        })
+    })
+    .catch(() => {
+        console.log("Deu alguma coisa")
+    })
+}
+
+nutriApp()
--- js/cards-instructor.js
+++ js/cards-instructor.js
@@ -21,40 +21,7 @@ function removeCardEventos() {
 removeCardEventos();
 
 
-//Button Top
-var scrollToButton = document.getElementById("scrollTopButton");
-var docElement = document.documentElement;
-
-function scrolltoTop(){
-    docElement.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-}
-
-function handleScroll(){
-    var scrollTotal = docElement.scrollHeight - docElement.clientHeight;
-    var count = docElement.scrollTop / scrollTotal
-    if(count > 0.1){
-        scrollToButton.classList.add("showBtn");
-    } else {
-        scrollToButton.classList.remove("showBtn");
-    }
-}
-
-document.addEventListener("scroll", handleScroll)
-scrollToButton.addEventListener("click", scrolltoTop);
 
 
-var hoje = new Date();
-
-var dia = hoje.getDate();
-var mes = hoje.getMonth() + 1; 
-
-if (dia === 25 && mes === 12) {
-    console.log("Merry Christmas!");
-} else {
-    console.log("Today is not Christmas.");
-}
 
 
