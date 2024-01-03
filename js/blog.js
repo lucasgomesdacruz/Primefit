d22574764691b93baf5fd9f050b367f479f3f7fa
Lucas Gomes <lucaass1997rp@gmail.com>
2024-01-03 13:51:49 -0300

    adição de novas funcionalidades Js e css

--- js/blog.js
+++ js/blog.js
@@ -1,38 +1,41 @@
-let listElement = document.querySelector("#app")
+let listElement = document.querySelector("#app");
+let loadingElement = document.createElement("p");
+loadingElement.textContent = "Carregando..."; // Texto exibido enquanto carrega
+listElement.appendChild(loadingElement);
 
-let posts = []
+let posts = [];
 
 function nutriApp() {
-
     fetch("https://sujeitoprogramador.com/rn-api/?api=posts")
-    .then((r) => r.json())
-    .then((json) => {
-        posts = json;
+        .then((r) => r.json())
+        .then((json) => {
+            posts = json;
+            listElement.removeChild(loadingElement); //remove o texto carregando 
 
-        posts.map((item) => {
-            let liElement = document.createElement("li")
-            let titleElement = document.createElement("h2")
-            let imgElement = document.createElement("img")
-            let descriptionElement = document.createElement("p")
+            posts.map((item) => {
+                let liElement = document.createElement("li");
+                let titleElement = document.createElement("h2");
+                let imgElement = document.createElement("img");
+                let descriptionElement = document.createElement("p");
 
-            let titleText = document.createTextNode(item.titulo)
-            titleElement.appendChild(titleText)
-            liElement.append(titleElement)
+                let titleText = document.createTextNode(item.titulo);
+                titleElement.appendChild(titleText);
+                liElement.append(titleElement);
 
-            imgElement.src = item.capa
-            liElement.appendChild(imgElement)
+                imgElement.src = item.capa;
+                liElement.appendChild(imgElement);
 
-            let descriptionText = document.createTextNode(item.subtitulo)
-            descriptionElement.appendChild(descriptionText)
-            liElement.appendChild(descriptionElement)
+                let descriptionText = document.createTextNode(item.subtitulo);
+                descriptionElement.appendChild(descriptionText);
+                liElement.appendChild(descriptionElement);
 
-            listElement.appendChild(liElement)
-
+                listElement.appendChild(liElement);
+            });
         })
-    })
-    .catch(() => {
-        console.log("Deu alguma coisa")
-    })
+        .catch(() => {
+            listElement.removeChild(loadingElement);
+            console.log("Deu alguma coisa");
+        });
 }
 
-nutriApp()
+nutriApp();
