fe70bbf1be35965d64b4e13ee2287859dd25241f
Lucas Gomes <lucaass1997rp@gmail.com>
2024-01-29 15:03:35 -0300

    Adição de novas funcionalidades Js e ajuste de responsividade

--- js/module/calculadora.js
+++ js/module/calculadora.js
@@ -19,21 +19,25 @@ export default function calculadora() {
     
             if (imc < 18.5) {
                 classification = 'Abaixo do peso';
+                resultado.style.color = 'red';
             } else if (imc < 25) {
                 classification = 'Peso normal';
+                resultado.style.color = 'green';
             } else if (imc < 30) {
                 classification = 'Acima do peso';
+                resultado.style.color = 'orange';
             } else if (imc < 35) {
                 classification = 'Obesidade Grau I';
+                resultado.style.color = 'red';
             } else if (imc < 41) {
                 classification = 'Obesidade Grau II';
+                resultado.style.color = 'red';
             } else {
                 classification = 'Obesidade Grau III';
+                resultado.style.color = 'red';
             }
-    
             altura.style.border = '';
             peso.style.border = '';
-            resultado.style.color = 'green';
             imc = imc.replace(/[.-]/g, ',');
             resultado.innerHTML = `IMC: ${imc} (${classification})`;
         } else {
@@ -42,7 +46,6 @@ export default function calculadora() {
             resultado.innerHTML = `Preencha os campos corretamente`;
             resultado.style.color = "red";
         }
-        
         altura.value = '';
         peso.value = '';
     
@@ -50,10 +53,8 @@ export default function calculadora() {
             altura.style.border = '';
             peso.style.border = '';
             resultado.innerHTML = '';
-        }, 7000);
+        }, 6000);
     };
-    
-    // Restante do código...
 }
 
 
